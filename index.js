8148be6ad34e776159aa8e40410bdf7e882ab68e
deoxxa <deoxxa@fknsrs.biz>
2016-05-23 12:08:37 +1000

    code import

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  rules: {
+    'no-prop-renaming': require('./rules/no-prop-renaming'),
+  },
+  rulesConfig: {
+    'no-prop-renaming': 0,
+  },
+};
---
+++ rules/no-prop-renaming.js
@@ -0,0 +1,37 @@
+var match = require('../lib/match');
+
+module.exports = function(context) {
+  return {
+    JSXAttribute: function(node) {
+      var passthrough = match(node, {
+        type: 'JSXAttribute',
+        name: {
+          type: 'JSXIdentifier',
+        },
+        value: {
+          type: 'JSXExpressionContainer',
+          expression: {
+            type: 'MemberExpression',
+            object: {
+              type: 'MemberExpression',
+              object: { type: 'ThisExpression' },
+              property: { type: 'Identifier', name: 'props' },
+            },
+            property: { type: 'Identifier' },
+          },
+        },
+      });
+
+      if (!passthrough) {
+        return;
+      }
+
+      var leftSide = node.name.name;
+      var rightSide = node.value.expression.property.name;
+
+      if (leftSide !== rightSide) {
+        context.report(node, 'prop ' + rightSide + ' passed as ' + leftSide);
+      }
+    },
+  };
+};
